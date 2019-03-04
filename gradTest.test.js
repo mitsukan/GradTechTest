74549ae82468e2d0dab58c59ada0dd1246c9f9aa
Jihin Ip <jim_ip20@hotmail.com>
2019-03-04 12:48:11 +0000

    splitFamily() splits parent into an array

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,30 +1,45 @@
-function createMenuData(data) {
+var splitParents = [];
 
+function createMenuData(data) {
+}
+
+function splitFamily(string){
+  splitted = string.split("/")
+  splitParents.push(splitted[0])
 }
 
 describe("menu Data Generator", () => {
-    it("creates correct data structure ", () => {
-      const data = [
-        "parent1/parent1child",
-        "parent1/parent1child2",
-        "parent2/parent2child",
-        "parent2/parent2child2",
-        "parent1/parent1child3",
-        "parent3",
-        "parent3/parent3child1",
-        "parent4"
-      ];
-  
-      const expectedResult = [
-        {
-          title: "parent1",
-          data: ["parent1child", "parent1child2", "parent1child3"]
-        },
-        { title: "parent2", data: ["parent2child", "parent2child2"] },
-        { title: "parent3", data: ["parent3child1"] }
-      ];
-  
-      const actualResult = createMenuData(data);
-      expect(actualResult).toMatchObject(expectedResult);
-    });
+
+  it("splits parent if it has a child ", () => {
+    const data = "parent1/parent1child";
+
+    const expectedResult = ["parent1"]
+    splitFamily(data)
+    expect(splitParents).toMatchObject(expectedResult);
+  });
+
+    // it("creates correct data structure ", () => {
+    //   const data = [
+    //     "parent1/parent1child",
+    //     "parent1/parent1child2",
+    //     "parent2/parent2child",
+    //     "parent2/parent2child2",
+    //     "parent1/parent1child3",
+    //     "parent3",
+    //     "parent3/parent3child1",
+    //     "parent4"
+    //   ];
+    //
+    //   const expectedResult = [
+    //     {
+    //       title: "parent1",
+    //       data: ["parent1child", "parent1child2", "parent1child3"]
+    //     },
+    //     { title: "parent2", data: ["parent2child", "parent2child2"] },
+    //     { title: "parent3", data: ["parent3child1"] }
+    //   ];
+    //
+    //   const actualResult = createMenuData(data);
+    //   expect(actualResult).toMatchObject(expectedResult);
+    // });
   });
