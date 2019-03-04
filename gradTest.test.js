68657ce9bce95b77ac5a13c8c143a5725d611d78
Jihin Ip <jim_ip20@hotmail.com>
2019-03-04 16:19:07 +0000

    Applying final test, removing return error that was disrupting flow of code

--- gradTest.test.js
+++ gradTest.test.js
@@ -5,6 +5,9 @@ var formattedFamilies = [];
 
 
 function createMenuData(data) {
+  splitFamily(data);
+  formatFamily();
+  return formattedFamilies;
 }
 
 
@@ -18,8 +21,6 @@ function splitFamily(array){
         splitParents.push(splitted[0]);
       }
       splitChildren.push(splitted[1]);
-    } else {
-      return Error('Parent has no child.');
     }
   }
 }
@@ -39,29 +40,31 @@ function formatFamily() {
   return formattedFamilies;
 }
 
-describe("splitFamily", () => {
-
-  it("splits family if it has a child ", () => {
-    const data = ["parent1/parent1child", "parent1/parent1child2", "parent2/parent2child"];
-    const expectedResult = ["parent1", "parent2"]
-    splitFamily(data)
-    expect(splitParents).toMatchObject(expectedResult);
-    expect(splitChildren).toMatchObject(["parent1child", "parent1child2", "parent2child"]);
-  });
-
-  it("Rejects parent if it has no child", () => {
-    const data = ["parent3"];
-    expect(splitFamily(data)).toMatchObject(Error("Parent has no child."));
-  });
-
-});
+// describe("splitFamily", () => {
+//
+//   it("splits family if it has a child ", () => {
+//     const data = ["parent1/parent1child", "parent1/parent1child2", "parent2/parent2child"];
+//     const expectedResult = ["parent1", "parent2"]
+//     splitFamily(data)
+//     expect(splitParents).toMatchObject(expectedResult);
+//     expect(splitChildren).toMatchObject(["parent1child", "parent1child2", "parent2child"]);
+//   });
+//
+//   it("Rejects parent if it has no child", () => {
+//     const data = ["parent3"];
+//     expect(splitFamily(data)).toMatchObject(Error("Parent has no child."));
+//   });
+//
+// });
 
 // describe("formatFamily", () => {
 //
 //   it("Formats a single family", () => {
-//     const data = ["parent1/parent1child", "parent1/parent1child2"];
+//     const data = ["parent1/parent1child", "parent1/parent1child2", "parent2/parent2child", "parent3/parent3child1"];
 //     splitFamily(data);
-//     expectedResult = [{title: "parent1", data:["parent1child", "parent1child2"]}];
+//     expectedResult = [{title: "parent1", data:["parent1child", "parent1child2"]},
+//     {title: "parent2", data: ["parent2child"]},
+//     {title: "parent3", data: ["parent3child1"]}];
 //     expect(formatFamily()).toMatchObject(expectedResult);
 //   });
 //
@@ -69,30 +72,28 @@ describe("splitFamily", () => {
 
 
 describe("menu Data Generator", () => {
+    it("creates correct data structure ", () => {
+      const data = [
+        "parent1/parent1child",
+        "parent1/parent1child2",
+        "parent2/parent2child",
+        "parent2/parent2child2",
+        "parent1/parent1child3",
+        "parent3",
+        "parent3/parent3child1",
+        "parent4"
+      ];
 
+      const expectedResult = [
+        {
+          title: "parent1",
+          data: ["parent1child", "parent1child2", "parent1child3"]
+        },
+        { title: "parent2", data: ["parent2child", "parent2child2"] },
+        { title: "parent3", data: ["parent3child1"] }
+      ];
 
-    // it("creates correct data structure ", () => {
-    //   const data = [
-    //     "parent1/parent1child",
-    //     "parent1/parent1child2",
-    //     "parent2/parent2child",
-    //     "parent2/parent2child2",
-    //     "parent1/parent1child3",
-    //     "parent3",
-    //     "parent3/parent3child1",
-    //     "parent4"
-    //   ];
-    //
-    //   const expectedResult = [
-    //     {
-    //       title: "parent1",
-    //       data: ["parent1child", "parent1child2", "parent1child3"]
-    //     },
-    //     { title: "parent2", data: ["parent2child", "parent2child2"] },
-    //     { title: "parent3", data: ["parent3child1"] }
-    //   ];
-    //
-    //   const actualResult = createMenuData(data);
-    //   expect(actualResult).toMatchObject(expectedResult);
-    // });
+      const actualResult = createMenuData(data);
+      expect(actualResult).toMatchObject(expectedResult);
+    });
   });
